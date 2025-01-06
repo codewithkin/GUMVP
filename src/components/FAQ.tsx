0b6bd8be0af79e6902841d0391176af9cb3a6155
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-06 06:15:53 +0200

    Update FAQ component to link to scheduling page for booking calls

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -14,7 +14,7 @@ export default function FAQ () {
                     <h2 className={`${montserrat.className} text-2xl text-center`}>Frequently Asked Questions</h2>
                     <p className="text-md text-dull">Quick and digestable answers to your most pressing questions, we reply within 24 hours.</p>
                 </article>
-                <Link href="" className="bg-charcoal text-white rounded-full px-8 py-2 flex justify-center items-center border border-primaryLight w-fit">Book a call</Link>
+                <Link target="_blank" href="https://calendly.com/admin-groundupmvp/project" className="bg-charcoal text-white rounded-full px-8 py-2 flex justify-center items-center border border-primaryLight w-fit">Book a call</Link>
             </article>
 
             {/* Questions */}
