ef9a0a285181f15600e415723efccb5a95ddac56
Kin Leon Zinzombe <kinzinzombe07@Gmail.com>
2025-01-06 06:06:56 +0200

    Fix Footer component layout by removing unnecessary whitespace

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
     return (
         <footer className="flex items-center justify-between py-4 px-10">
             <h2 className={`${montserrat.className} text-2xl text-center`}>GroundUp<span className={poppins.className}>MVP</span></h2>
-        
+
             <Link href="https://x.com/groundupmvp" target="_blank">
                 <FaXTwitter size={24} />
             </Link>
